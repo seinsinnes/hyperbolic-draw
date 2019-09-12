f594d1935a8fe862680207c7d8729d9e14eaaa80
Christopher Day <me@christopherrday.com>
2019-09-12 23:39:41 +1000

    Finding points of lines on the infinite circle boundary.

--- src/hyperbolic_draw/core.cljs
+++ src/hyperbolic_draw/core.cljs
@@ -42,6 +42,22 @@
        (/ (- (* v0 (+ umag 1)) (* u0 (+ vmag 1))) divisor)]
     )))
 
+(defn poincareInfinityPointsFromGeodesic [center radius]
+  (let [xc  (first center)
+        yc (second center)]
+    (let [dSqrd  (+ (q/sq xc)  (q/sq yc))]
+      (let [d (q/sqrt dSqrd)
+            inter (+ (- dSqrd (q/sq radius)) 1.0)]
+        (let [x (/ inter (* 2 d))
+              y (q/sqrt (- 1.0 (q/sq x)))
+              angle (q/acos (/ xc d))]
+          (let [xr1 (- (* x (q/cos angle)) (* y (q/sin angle)))
+                yr1 (+ (* x (q/sin angle)) (* y (q/cos angle)))
+                xr2 (+ (* x (q/cos angle)) (* y (q/sin angle)))
+                yr2 (- (* x (q/sin angle)) (* y (q/cos angle)))]
+            ;(println [xr1 yr1 xr2 yr2])
+            (if (< yc 0) [{:pos [xr1 (- yr1)]} {:pos [xr2 (- yr2)]}] [{:pos [xr1 yr1]} {:pos [xr2 yr2]}])))))))
+
 (defn poincareArcAngles [pt1 pt2 center]
   (let [ angles (sort [(q/atan2 (- (second pt1) (second center)) (- (first pt1) (first center)))
    (q/atan2 (- (second pt2) (second center)) (- (first pt2) (first center)))]) ]
@@ -59,6 +75,38 @@
     (assoc state :points (conj (:points state) {:pos index }))
     state))
 
+(defn addPt [pt1 pt2]
+  {:pos [(+ (first pt1) (first (:pos pt2))) (+ (second pt1) (second (:pos pt2)))]}
+  )
+(defn math-add [a b]
+  (.add js/math a b))
+
+(defn math-multiply [a b]
+  (.multiply js/math a b))
+
+(defn math-divide [a b]
+  (.divide js/math a b))
+
+(defn moebius [a b c d pt]
+  (let [cpt (.complex js/math (first (:pos pt)) (second (:pos pt)))]
+    (let [w (math-divide (math-add (math-multiply a cpt) b) (math-add (math-multiply c cpt) d))]
+      {:pos [(aget w "re") (aget w "im")]}
+      )
+  )
+)
+
+
+(defn mouse-dragged [state event]
+  (let [ end-pt (from-screenspace-to-normalspace [(:x event) (:y event)] )
+         start-pt (from-screenspace-to-normalspace [(:p-x event) (:p-y event)] )]
+    (let [ dx ( - (first end-pt) (first start-pt))
+          dy ( - (second end-pt) (second start-pt))]
+      ;(println (moebius 2 1 -1 2 {:pos end-pt}))
+      ;(println (:points state))
+      (assoc state :points (map (partial moebius 1 0.2 0.2 1) (:points state)))
+      ;(assoc state :points (map (partial addPt [dx dy]) (:points state) ))
+      )))
+
 (defn draw-geodesic [pt1 pt2]
   (let [spt1  (from-normalspace-to-screenspace pt1)
         spt2 (from-normalspace-to-screenspace pt2)]
@@ -72,8 +120,11 @@
       (let [scenter (from-normalspace-to-screenspace center)
             radius (poincareArcRadiusFromCenter center)
             angles (poincareArcAngles pt1 pt2 center)]
+        (let [bpts (poincareInfinityPointsFromGeodesic center radius)] 
+          (q/ellipse (first (from-normalspace-to-screenspace  (:pos (first bpts)))) (second (from-normalspace-to-screenspace  (:pos (first bpts)))) point-size point-size)
+          (q/ellipse (first (from-normalspace-to-screenspace  (:pos (second bpts)))) (second (from-normalspace-to-screenspace (:pos (second bpts)))) point-size point-size))
         (q/arc (first scenter) (second scenter) (* radius (/ (q/width) 2)) (* radius (/ (q/width) 2)) (first angles) (second angles)
-          )))))
+               )))))
 
 (defn draw-state [state]
   (q/no-stroke)
@@ -95,5 +146,6 @@
      :size [500 500]
      :setup setup
      :mouse-clicked mouse-clicked
+     :mouse-dragged mouse-dragged
      :draw draw-state
      :middleware [m/fun-mode]))
